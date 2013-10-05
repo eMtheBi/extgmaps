50d72b119760d8b9329bb02e90469705c13da2e1
Markus Bloch <bloch@ic-tourismus.de>
2013-10-05 12:22:56 +0200

    update js for map tool

--- Hooks/TceMap.php
+++ Hooks/TceMap.php
@@ -40,17 +40,22 @@ class TceMap {
 	 * @return string
 	 */
 	public function displayMap($incomingData)    {
+
 		$table = $incomingData['table'];
 		$field = $incomingData['field'];
 		$row = $incomingData['row'];
 		$this->pObj = $incomingData['pObj'];
-		if ($row['latitude'] == 0) $row['latitude'] = 53.238638116487536;
-		if ($row['longitude'] == 0) $row['longitude'] = 7.503833770751953;
+		if ($row['latitude'] == 0) $row['latitude'] = 53.4136310851194;
+		if ($row['longitude'] == 0) $row['longitude'] = 7.2233819961528525;
 		$content = '<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>';
+		$content .= '<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>';
+
+
 		$content .= '<div id="mapdiv" style="width:600px; height:400px;"></div>';
 		$content .= '
 			<script type="text/javascript">
 
+				var jQuery2 = jQuery.noConflict();
 				var mapReloaded = false;
 				var latlng = new google.maps.LatLng('.$row['latitude'].', '.$row['longitude'].');
 				var myOptions = {
@@ -72,6 +77,29 @@ class TceMap {
 					updateFields(myMarker.getPosition());
 				});
 
+				var tabIndexForMap = 0;
+				var tabName = "";
+				// get all tabs
+				jQuery2(".c-tablayer").each(function() {
+					tabName = jQuery2(this).attr("id").substring(0,jQuery2(this).attr("id").length-3);
+					var parts = tabName.split("-");
+					tabName += "MENU";
+
+					// search for geo tab
+					var tabFound = jQuery2("#" + tabName).find("a").html().search(/GeoVerortung/);
+					if (tabFound != -1) {
+						// get tab index from id name
+						tabIndexForMap = parts[2];
+					}
+					// get current tab
+					currentTab = top.DTM_currentTabs[jQuery2(this).attr("id").substring(0,jQuery2(this).attr("id").length-6)]
+				});
+
+				jQuery2("#" + tabName).on("click",function(){
+					google.maps.event.trigger(myMap, "resize");
+					myMap.setCenter(latlng);
+				});
+
 				function updateFields(position){
 					document.getElementsByName("data[' . $table . ']['.$row['uid'].'][latitude]_hr")[0].value = parseFloat(position.lat());
 					typo3form.fieldGet("data[' . $table . ']['.$row['uid'].'][latitude]","","","'.$row['uid'].'","");
@@ -80,6 +108,7 @@ class TceMap {
 					typo3form.fieldGet("data[' . $table . ']['.$row['uid'].'][longitude]","","","'.$row['uid'].'","");
 					TBE_EDITOR.fieldChanged("' . $table . '","'.$row['uid'].'","longitude","data[tx_poi_poi]['.$row['uid'].'][longitude]");
 				}
+
 			</script>';
 
 		return $content;
