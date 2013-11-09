de5032349b539157eec702352ce9ad31af114301
Markus <markus@emthebi.de>
2013-11-09 20:06:51 +0100

    update for map overflow

--- Resources/Public/Js/fullSizeMap.js
+++ Resources/Public/Js/fullSizeMap.js
@@ -1,13 +1,20 @@
 var mapIsInitialised = false;
 var scrollPosition = 0;
+var defaultOverflow = 'auto';
 jQuery('.js_toogleMapContainer').click(function() {
 
+	defaultOverflow = jQuery("html").css('overflow');
+
+	jQuery("html").css("overflow", 'hidden');
 
 	scrollPosition = jQuery(window).scrollTop();
 	if(mapIsInitialised === false) {
 		resizeMap(true);
 		buildMap();
 	}
+
+	extGoogleMap.setOptions({scrollwheel:false});
+
 	jQuery('.js_fullSizeMapContainer').show();
 	jQuery('.js_fullScreeButton').show();
 	jQuery(window).scrollTop(0);
@@ -18,6 +25,8 @@ jQuery('.js_fullScreeButton').click(function() {
 	jQuery(window).scrollTop(scrollPosition);
 	jQuery('.js_fullScreeButton').hide();
 	jQuery('.js_fullSizeMapContainer').hide();
+	extGoogleMap.setOptions({scrollwheel:true});
+	jQuery("html").css("overflow", defaultOverflow);
 });
 
 jQuery(window).resize(function() {
